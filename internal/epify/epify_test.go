694be4dce0e8368f7f1c3d75e84dc4f14e4d5adc
Matthew Dargan <matthewdargan57@gmail.com>
2024-06-24 22:19:54 -0500

    test(season): add valid episode cases

--- internal/epify/epify_test.go
+++ internal/epify/epify_test.go
@@ -69,6 +69,7 @@ func TestMkSeason(t *testing.T) {
 		name    string
 		season  *Season
 		wantErr bool
+		create  bool
 	}{
 		{
 			name:    "invalid season number",
@@ -77,11 +78,11 @@ func TestMkSeason(t *testing.T) {
 		},
 		{
 			name:    "invalid show directory",
-			season:  &Season{N: "3", ShowDir: "nonexistent-dir"},
+			season:  &Season{N: "3", ShowDir: "nonexistentdir"},
 			wantErr: true,
 		},
 		{
-			name:    "show directory not a directory",
+			name:    "show file",
 			season:  &Season{N: "3", ShowDir: "doc.go"},
 			wantErr: true,
 		},
@@ -96,9 +97,50 @@ func TestMkSeason(t *testing.T) {
 			wantErr: true,
 		},
 		{
-			name:    "invalid episode directory",
-			season:  &Season{N: "3", Episodes: []string{"nonexistent-dir"}},
+			name:    "episode directory",
+			season:  &Season{N: "3", Episodes: []string{"epdir"}},
 			wantErr: true,
+			create:  true,
+		},
+		{
+			name:    "episode without number",
+			season:  &Season{N: "3", Episodes: []string{"epx.mkv"}},
+			wantErr: true,
+			create:  true,
+		},
+		{
+			name: "valid season 3",
+			season: &Season{N: "3", Episodes: []string{
+				"ep1.mkv", "ep2.mkv", "ep3.mkv", "ep4.mkv", "ep5.mkv",
+				"ep6.mkv", "ep7.mkv", "ep8.mkv", "ep9.mkv", "ep10.mkv",
+				"ep11.mkv", "ep12.mkv", "ep13.mkv", "ep14.mkv", "ep15.mkv",
+				"ep16.mkv", "ep17.mkv", "ep18.mkv", "ep19.mkv", "ep20.mkv",
+				"ep21.mkv", "ep22.mkv", "ep23.mkv", "ep24.mkv", "ep25.mkv",
+				"ep26.mkv", "ep27.mkv", "ep28.mkv", "ep29.mkv", "ep30.mkv",
+				"ep31.mkv", "ep32.mkv", "ep33.mkv", "ep34.mkv", "ep35.mkv",
+				"ep36.mkv", "ep37.mkv", "ep38.mkv", "ep39.mkv", "ep40.mkv",
+				"ep41.mkv", "ep42.mkv", "ep43.mkv", "ep44.mkv", "ep45.mkv",
+				"ep46.mkv", "ep47.mkv", "ep48.mkv", "ep49.mkv", "ep50.mkv",
+				"ep51.mkv", "ep52.mkv", "ep53.mkv", "ep54.mkv", "ep55.mkv",
+				"ep56.mkv", "ep57.mkv", "ep58.mkv", "ep59.mkv", "ep60.mkv",
+				"ep61.mkv", "ep62.mkv", "ep63.mkv", "ep64.mkv", "ep65.mkv",
+				"ep66.mkv", "ep67.mkv", "ep68.mkv", "ep69.mkv", "ep70.mkv",
+				"ep71.mkv", "ep72.mkv", "ep73.mkv", "ep74.mkv", "ep75.mkv",
+				"ep76.mkv", "ep77.mkv", "ep78.mkv", "ep79.mkv", "ep80.mkv",
+				"ep81.mkv", "ep82.mkv", "ep83.mkv", "ep84.mkv", "ep85.mkv",
+				"ep86.mkv", "ep87.mkv", "ep88.mkv", "ep89.mkv", "ep90.mkv",
+				"ep91.mkv", "ep92.mkv", "ep93.mkv", "ep94.mkv", "ep95.mkv",
+				"ep96.mkv", "ep97.mkv", "ep98.mkv", "ep99.mkv", "ep100.mkv",
+				"ep101.mkv",
+			}},
+			wantErr: false,
+			create:  true,
+		},
+		{
+			name:    "valid season 11",
+			season:  &Season{N: "11", Episodes: []string{"ep9.mp4", "ep10.mp4"}},
+			wantErr: false,
+			create:  true,
 		},
 	}
 	for _, tt := range tests {
@@ -112,10 +154,36 @@ func TestMkSeason(t *testing.T) {
 				defer os.RemoveAll(dir)
 				tt.season.ShowDir = dir
 			}
+			if tt.create {
+				dir, err := os.MkdirTemp("", "season")
+				if err != nil {
+					t.Fatal(err)
+				}
+				defer os.RemoveAll(dir)
+				createEpisodes(t, dir, tt.season.Episodes)
+			}
 			err := MkSeason(tt.season)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("MkSeason(%v) error = %v", tt.season, err)
 			}
 		})
+	}
+}
+
+func createEpisodes(t *testing.T, seasonDir string, eps []string) {
+	for i, e := range eps {
+		path := filepath.Join(seasonDir, e)
+		eps[i] = path
+		if filepath.Ext(path) == "" {
+			if err := os.Mkdir(path, 0o755); err != nil {
+				t.Fatal(err)
+			}
+			continue
+		}
+		f, err := os.Create(path)
+		if err != nil {
+			t.Fatal(err)
+		}
+		f.Close()
 	}
 }
