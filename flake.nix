02ad4cf5707bd6e0e13a9bfa47a6a01000c58787
Matthew Dargan <matthewdargan57@gmail.com>
2024-10-02 19:43:35 -0500

    build(nix): nix flake update

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
           pname = "epify";
           src = ./.;
           vendorHash = "sha256-xzbAmWhfOiFYnMAVGgqDQzPLqK5/Gg6oCeSuJ9I4OCE=";
-          version = "0.4.2";
+          version = "0.4.3";
         };
         pre-commit = {
           check.enable = false;
